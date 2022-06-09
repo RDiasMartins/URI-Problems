fe1ef8330e3413afe03d2d37915f0d9eb21fd424
RDiasMartins <renan2204@outlook.com>
2022-06-08 21:21:07 -0300

    Updates Exercises

--- Iniciante/C#/1012_Area.cs
+++ Iniciante/C#/1012_Area.cs
@@ -3,10 +3,9 @@ using System;
 class URI {
 
     static void Main(string[] args) { 
-        string[] texto = Console.ReadLine().Split(" ");
-        double n1 = Convert.ToDouble(texto[0]);
-        double n2 = Convert.ToDouble(texto[1]);
-        double n3 = Convert.ToDouble(texto[3]);
+        double a = Convert.ToDouble(Console.ReadLine());
+        double b = Convert.ToDouble(Console.ReadLine());
+        double c = Convert.ToDouble(Console.ReadLine());
 
         double triangulo = (a*c)/2;
         double circulo = Math.Pow(c, 2)*3.14159;
@@ -14,8 +13,7 @@ class URI {
         double quadrado = Math.Pow(b,2);
         double retangulo = a*b;
 
-        Console.WriteLine("TRIANGULO: "+triangulo.ToString("F3")
-            +"\nCIRCULO: "+circulo.ToString("F3")
+        Console.WriteLine("TRIANGULO: "+triangulo.ToString("F3")+"\nCIRCULO: "+circulo.ToString("F3")
             +"\nTRAPEZIO: "+trapezio.ToString("F3")
             +"\nQUADRADO: "+quadrado.ToString("F3")
             +"\nRETANGULO: "+retangulo.ToString("F3"));
